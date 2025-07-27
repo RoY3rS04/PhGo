71b48c3c44e96668f3f711c604f2bc973c2fd127
Roger Osorio <rogermoises2004@gmail.com>
2025-07-26 21:07:11 -0600

    first implementation of the canonicalHeaderKey helper function

--- src/Http/Header.php
+++ src/Http/Header.php
@@ -4,6 +4,16 @@ namespace Royers\Http;
 
 class Header
 {
+    private const array COMMON_HEADERS = [
+      "content_type",
+      "content_length",
+      "redirect_http_authorization"
+    ];
+
+    private const array VALID_HEADER_SEPARATORS = [
+      "!", "#", "$", "%", "&", "'", "*", "+", "-", ".", "^", "_", "|", "~"
+    ];
+
     public array $headers = [];
 
     public function __construct()
@@ -12,29 +22,56 @@ class Header
         $this->headers = $headers != false ? $headers : $this->getHeaders();
     }
 
+    public static function canonicalHeaderKey(string $s): string
+    {
+        $separator = '';
+
+        $containsValidSeparator = false;
+
+        foreach (self::VALID_HEADER_SEPARATORS as $spr) {
+            if (str_contains($s, $spr)) {
+                $containsValidSeparator = true;
+                $separator = $spr;
+                break;
+            }
+        }
+
+        if (!$containsValidSeparator) {
+            return $s;
+        }
+
+        $keyParts = explode($separator, $s);
+
+        $newKeyParts = [];
+
+        foreach ($keyParts as $kp) {
+            $newKeyParts[] = ucfirst($kp);
+        }
+
+        $headerKey = implode('-', $newKeyParts);
+
+        return $headerKey;
+    }
+
     private function getHeaders(): array
     {
         $reqHeaders = [];
 
         foreach ($_SERVER as $k => $v) {
-            $headerKey = strtolower($k);
+            $key = strtolower($k);
+            $isHttpPrefixed = str_starts_with($key, 'http');
 
-            if (str_starts_with($headerKey, 'http')) {
+            if ($isHttpPrefixed
+                || in_array($key, self::COMMON_HEADERS)
+            ) {
 
-                $keyParts = explode('_', $headerKey);
+                $canonicKey = self::canonicalHeaderKey($key);
 
-                $newKeyParts = [];
-
-                foreach ($keyParts as $kp) {
-
-                    if ($kp == 'http') {
-                        continue;
-                    }
-
-                    $newKeyParts[] = ucfirst($kp);
+                if ($isHttpPrefixed) {
+                    $headerKey = str_replace('Http-', '', $canonicKey);
+                } else {
+                    $headerKey = $canonicKey;
                 }
-
-                $headerKey = implode('-', $newKeyParts);
 
                 $reqHeaders[$headerKey] = $v;
             }
